a2bc3712b3f9305ab986c9f8045910e536964ef8
nrain78 <noorrafique7890>
2025-07-26 21:22:36 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const adminRoutes = require("./routes/adminRoutes");
 const app = express();
 // CORS middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://std-feedback-x4zu.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
